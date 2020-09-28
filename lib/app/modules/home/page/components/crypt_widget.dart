edfc031f2b3a8339466a89b77fc735487c6008ab
Rodrigo Liporace <rodrigo.vasquestuf@gmail.com>
2020-09-28 19:34:06 -0300

    Clipboard
    Added clipboard funcionality to the app

--- lib/app/modules/home/page/components/crypt_widget.dart
+++ lib/app/modules/home/page/components/crypt_widget.dart
@@ -1,3 +1,4 @@
+import 'package:clipboard_manager/clipboard_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:redes_av1/app/core/model/crypt_model.dart';
@@ -9,38 +10,48 @@ class CryptWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(15)),
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  cryptModel.cypher.toString(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: NeumorphicTheme.of(context).current.accentColor,
-                      fontSize: 16),
-                ),
-                Text(cryptModel.createdAt)
-              ],
-            ),
-            Text(
-              "Operação: ${(cryptModel.isEnrypt) ? "encrypt" : "decrypt"}",
-              style: TextStyle(fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text("Chave: ${cryptModel.key}"),
-            Text("Mensagem: ${cryptModel.message}"),
-            Text("Resultado: ${cryptModel.result}")
-          ],
+    return InkWell(
+      onTap: (){
+        ClipboardManager.copyToClipBoard(cryptModel.result).then((result) {
+          final snackBar = SnackBar(
+            content: Text('Resultado copiado para a clipboard'),
+          );
+          Scaffold.of(context).showSnackBar(snackBar);
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(15)),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    cryptModel.cypher.toString(),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: NeumorphicTheme.of(context).current.accentColor,
+                        fontSize: 16),
+                  ),
+                  Text(cryptModel.createdAt)
+                ],
+              ),
+              Text(
+                "Operação: ${(cryptModel.isEnrypt) ? "encrypt" : "decrypt"}",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text("Chave: ${cryptModel.key}"),
+              Text("Mensagem: ${cryptModel.message}"),
+              Text("Resultado: ${cryptModel.result}")
+            ],
+          ),
         ),
       ),
     );
